4060dd90915836fab941e0420b784da80e23cccf
Ahmad Kamal B Norizan <kamalnorizan@gmail.com>
2020-03-03 15:17:19 +0800

    model certs

--- app/Cert.php
+++ app/Cert.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Cert extends Model
 {
-    //
+    public $timestamps = true;
+
+    protected $table = 'certs';
+
+    protected $primaryKey = 'id';
+
+    public $incrementing = true;
+
+    protected $guarded = ['id'];
 }
